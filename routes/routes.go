af5f07c804ae063fe581bdd83a727c940f125f77
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-02-03 23:36:54 +0300

    routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,16 @@
-package main
+package routes
 
-import {
-	"fmt"
-	"http"
-	"net/http"
+import (
+	"github.com/akhil/ecommerce-yt/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.POST("/users/signup", controllers.signup)
+	incomingRoutes.POST("/users/login", controllers.login)
+	incomingRoutes.POST("/admin/addproduct", controllers.addproduct)
+	incomingRoutes.GET("/users/productview", controllers.productview)
+	incomingRoutes.GET("/users/search", controllers.search)
 }
+
+
