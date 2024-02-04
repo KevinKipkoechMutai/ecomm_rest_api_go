762594dd7c61c08f7d6b96ce3135fb46eed0b413
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2024-02-04 23:21:29 +0300

    routes

--- routes/routes.go
+++ routes/routes.go
@@ -6,11 +6,11 @@ import (
 )
 
 func UserRoutes(incomingRoutes *gin.Engine){
-	incomingRoutes.POST("/users/signup", controllers.signup)
-	incomingRoutes.POST("/users/login", controllers.login)
-	incomingRoutes.POST("/admin/addproduct", controllers.addproduct)
-	incomingRoutes.GET("/users/productview", controllers.productview)
-	incomingRoutes.GET("/users/search", controllers.search)
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
 }
 
 
